e3b167a8e2c4dc71156a0fbc50230b5107d92f35
AlexMercer12138 <alexmercer@outlook.com>
2026-09-02 09:55:51 +0800

    test: cover binary64 runtime catalog entry

--- merc32-vsce/scripts/test-runtime-startup.js
+++ merc32-vsce/scripts/test-runtime-startup.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const { loadRuntimeObjects } = require('../out/runtime/runtimeCatalog');
 const objects = loadRuntimeObjects();
-assert.strictEqual(objects.length, 3);
+assert.strictEqual(objects.length, 4);
 assert(objects.every(object => object.abi === 'merc32-c-v1'));
 console.log('runtime startup tests passed');
